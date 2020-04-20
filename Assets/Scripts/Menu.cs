72b59c626f82040b09ff9eba00c92e3e1a268c74
Iker <ikernoya4@gmail.com>
2020-04-20 08:59:13 -0300

    Buttons now work in the menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void onClickA()
     {
-        
+        SceneManager.LoadScene("Game");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void OnClickB()
     {
-        
+        SceneManager.LoadScene("Credits");
     }
 }
